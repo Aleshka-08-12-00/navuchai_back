bf09acfef647aa6fbad7331b626e3d044929f273
Лобановский Владислав <v.lobanovskiy@sellwin.by>
2025-04-14 13:12:59 +0000

    CORS fix

--- navuchai_api/app/main.py
+++ navuchai_api/app/main.py
@@ -3,8 +3,17 @@ from fastapi import FastAPI
 from app.config import engine
 from app.models import Base
 from app.routes import tests, questions
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Test API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(tests.router)
 app.include_router(questions.router)
