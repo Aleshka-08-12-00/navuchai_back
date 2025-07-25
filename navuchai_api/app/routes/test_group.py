54c9e2013e4af43a19da10d614af3b8217a3aab5
Лобановский Владислав <v.lobanovskiy@sellwin.by>
2025-07-25 10:08:35 +0300

    Добавлены /

--- navuchai_api/app/routes/test_group.py
+++ navuchai_api/app/routes/test_group.py
@@ -12,6 +12,7 @@ from app.exceptions import NotFoundException, DatabaseException
 
 router = APIRouter(prefix="/api/test-groups", tags=["Test groups"])
 
+
 @router.get("/", response_model=List[TestGroupEnriched])
 async def list_test_groups(db: AsyncSession = Depends(get_db), user=Depends(authorized_required)):
     try:
@@ -19,7 +20,8 @@ async def list_test_groups(db: AsyncSession = Depends(get_db), user=Depends(auth
     except SQLAlchemyError:
         raise DatabaseException("Ошибка при получении списка групп")
 
-@router.get("/{group_id}", response_model=TestGroupEnriched)
+
+@router.get("/{group_id}/", response_model=TestGroupEnriched)
 async def get_test_group(group_id: int, db: AsyncSession = Depends(get_db), user=Depends(authorized_required)):
     try:
         group = await crud.get_test_group(db, group_id)
@@ -45,15 +47,19 @@ async def get_test_group(group_id: int, db: AsyncSession = Depends(get_db), user
     except SQLAlchemyError:
         raise DatabaseException("Ошибка при получении группы")
 
+
 @router.post("/", response_model=TestGroup)
-async def create_test_group(data: TestGroupCreate, db: AsyncSession = Depends(get_db), user=Depends(admin_moderator_required)):
+async def create_test_group(data: TestGroupCreate, db: AsyncSession = Depends(get_db),
+                            user=Depends(admin_moderator_required)):
     try:
         return await crud.create_test_group(db, data)
     except SQLAlchemyError:
         raise DatabaseException("Ошибка при создании группы")
 
-@router.put("/{group_id}", response_model=TestGroup)
-async def update_test_group(group_id: int, data: TestGroupUpdate, db: AsyncSession = Depends(get_db), user=Depends(admin_moderator_required)):
+
+@router.put("/{group_id}/", response_model=TestGroup)
+async def update_test_group(group_id: int, data: TestGroupUpdate, db: AsyncSession = Depends(get_db),
+                            user=Depends(admin_moderator_required)):
     try:
         return await crud.update_test_group(db, group_id, data)
     except NotFoundException as e:
@@ -61,7 +67,8 @@ async def update_test_group(group_id: int, data: TestGroupUpdate, db: AsyncSessi
     except SQLAlchemyError:
         raise DatabaseException("Ошибка при обновлении группы")
 
-@router.delete("/{group_id}", response_model=TestGroup)
+
+@router.delete("/{group_id}/", response_model=TestGroup)
 async def delete_test_group(group_id: int, db: AsyncSession = Depends(get_db), user=Depends(admin_moderator_required)):
     try:
         return await crud.delete_test_group(db, group_id)
@@ -70,18 +77,21 @@ async def delete_test_group(group_id: int, db: AsyncSession = Depends(get_db), u
     except SQLAlchemyError:
         raise DatabaseException("Ошибка при удалении группы")
 
-@router.post("/add-test", response_model=TestGroupTest)
-async def add_test_to_group(data: TestGroupTestCreate, db: AsyncSession = Depends(get_db), user=Depends(admin_moderator_required)):
+
+@router.post("/add-test/", response_model=TestGroupTest)
+async def add_test_to_group(data: TestGroupTestCreate, db: AsyncSession = Depends(get_db),
+                            user=Depends(admin_moderator_required)):
     try:
         return await crud.add_test_to_group(db, data)
     except SQLAlchemyError:
         raise DatabaseException("Ошибка при добавлении теста в группу")
 
-@router.get("/{group_id}/tests", response_model=List[TestWithDetails])
+
+@router.get("/{group_id}/tests/", response_model=List[TestWithDetails])
 async def get_tests_by_group_id(
-    group_id: int,
-    db: AsyncSession = Depends(get_db),
-    user = Depends(authorized_required)
+        group_id: int,
+        db: AsyncSession = Depends(get_db),
+        user=Depends(authorized_required)
 ):
     try:
         return await crud.get_tests_by_group_id(db, group_id)
