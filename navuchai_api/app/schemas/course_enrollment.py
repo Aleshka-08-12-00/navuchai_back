2ab1a332b7b5390dd1fb28297ebc1515a0710cdb
Лобановский Владислав <v.lobanovskiy@sellwin.by>
2025-08-13 11:33:35 +0300

    добавлен метод get /api/courses/users/courses/grouped/

--- navuchai_api/app/schemas/course_enrollment.py
+++ navuchai_api/app/schemas/course_enrollment.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from pydantic import BaseModel
+from typing import List, Optional
 
 class CourseEnrollmentBase(BaseModel):
     id: int
@@ -8,3 +9,32 @@ class CourseEnrollmentBase(BaseModel):
     enrolled_at: datetime
     class Config:
         from_attributes = True
+
+
+class FileInfo(BaseModel):
+    id: int
+    type: Optional[str]
+    name: str
+    size: int
+    path: str
+    provider: Optional[str]
+    created_at: datetime
+    updated_at: datetime
+    creator_id: int
+    class Config:
+        from_attributes = True
+
+
+class UserCourseInfo(BaseModel):
+    course_id: int
+    course_title: str
+    enrolled_at: datetime
+
+
+class UserCoursesGrouped(BaseModel):
+    user_id: int
+    user_name: str
+    user_email: str
+    user_img: Optional[FileInfo]
+    user_thumbnail: Optional[FileInfo]
+    courses: List[UserCourseInfo]
