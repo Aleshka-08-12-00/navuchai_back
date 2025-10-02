80f48d17c6373823724d663157e1f01e368504f3
Лобановский Владислав <v.lobanovskiy@sellwin.by>
2025-10-02 10:48:30 +0300

    Фикс паролей

--- navuchai_api/app/auth.py
+++ navuchai_api/app/auth.py
@@ -8,20 +8,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def verify_password(plain_password, hashed_password):
-    # Обрезаем пароль до 72 байт для совместимости с bcrypt
-    if isinstance(plain_password, str):
-        plain_password = plain_password.encode('utf-8')
-    if len(plain_password) > 72:
-        plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password):
-    # Обрезаем пароль до 72 байт для совместимости с bcrypt
-    if isinstance(password, str):
-        password = password.encode('utf-8')
-    if len(password) > 72:
-        password = password[:72]
     return pwd_context.hash(password)
 
 
